de028083a3f99232c65b17ecd2f5521694ea2a8b
Ganesh <ashok606@gmail.com>
2021-11-25 17:16:10 +0530

    Added HttpErrors package

--- utils/errors.js
+++ utils/errors.js
@@ -1,9 +1,9 @@
+import httpErrors from 'http-errors'
+
 import logger from './logger.js'
 
 const pathNotFound = (req, res, next) => {
-    const error = new Error(`Path Not Found -> ${req.url}`)
-    error.status = 404
-    next(error)
+    next(httpErrors.NotFound(`Path Not Found -> ${req.url}`))
 }
 
 const errorHandler = (error, _req, res, _next) => {
